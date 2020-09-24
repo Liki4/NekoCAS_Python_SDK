3c412dd7183b78871f760da4d058c653aee81228
E99p1ant <i@github.red>
2020-09-24 16:38:56 +0800

    v0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='NekoCAS',
-    version='0.1',
+    version='0.2',
     description='NekoCAS SDK for Python',
     url='https://github.com/NekoWheel/NekoCAS_Python_SDK',
     author='E99p1ant',
